92464ecaf852339d60e22ae83da0b850d30bf6fe
alekstrselicc <aleks.trselic@student.um.si>
2022-04-06 11:54:50 +0200

    main is responsive and drawers for mobile are working

--- web/text-app/src/views/MainView.vue
+++ web/text-app/src/views/MainView.vue
@@ -1,42 +1,137 @@
 <template>
-    <v-main>
-      <v-container class="fill-height justify-center">
-        <v-row>
-          <!-- playgrounds -->
-          <v-col sm="3">
-            <v-sheet rounded="lg" min-height="100%" class="playground_sheet sheet_color">
-            </v-sheet>
-          </v-col>
+    <v-container class="fill-height justify-center first_main">
+      <v-layout class="main_layout">
+
+          <!-- This is for playgrounds -->
+          <v-flex lg3 class="hidden-sm-and-down playground" sm3>
+
+          </v-flex>
 
 
-          <!-- chat -->
-          <v-col sm="7">
-            <v-sheet min-height="92vh" rounded="lg" class="chat_sheet sheet_color">   
-            </v-sheet>
-          </v-col>
+          <v-navigation-drawer v-model="drawer1" app class="hidden-md-and-up playground_drawer" color="black">
+            playgrounds
+
+          </v-navigation-drawer>
 
 
-          <!-- friends -->
-          <v-col sm="2">
-            <v-sheet rounded="lg" min-height="100%" class="friends_sheet sheet_color">
+          <!-- This is for chat -->
+          <v-flex lg7 class="chat" sm7>
+          
+          <!-- toolbar with burger menu and friends icon -->
+          <v-app-bar color="transparent" class="hidden-md-and-up small_bar" flat>
 
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
+            <v-app-bar-nav-icon @click="drawer1 = true" x-large color="white"></v-app-bar-nav-icon>
+
+            <v-spacer></v-spacer>
+
+            <v-app-bar-nav-icon @click="drawer2 = true" x-large color="white"></v-app-bar-nav-icon>
+
+          </v-app-bar>
+
+
+          </v-flex>
+
+
+          <v-navigation-drawer v-model="drawer2" app right class="hidden-md-and-up friends_drawer" color="black">
+              friends
+          </v-navigation-drawer>
+
+
+
+          <!-- This is for friends -->
+          <v-flex lg2 class="hidden-sm-and-down friends" sm2>
+
+
+          </v-flex>
+
+
+
+      </v-layout>
+    </v-container>
 </template>
 
-<script>
+<script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({
+    data(){
+      return {
+        drawer1: false,
+        drawer2: false,  
+      }
+    }
+  })
+
 
 </script>
 
 <style>
 
+  /* Main stuff for good looks */
 
+
+
+    .first_main{
+      max-width: 2500px !important;
+    }
+    .playground{
+     
+      border-radius: 30px;
+      background-color: rgba(0, 0, 0, 0.4) !important;
+    }
+    .chat{
+      
+      border-radius: 30px;
+      margin-left: 10px;
+      background-color: rgba(0, 0, 0, 0.4) !important;
+    }
+    .friends{
+      
+      border-radius: 30px;
+      margin-left: 10px;
+      background-color: rgba(0, 0, 0, 0.4) !important;
+    }
+
+    .small_bar{
+      display: none;
+    }
+
+    /* This is the small navbar in mobile mode */
+    @media (max-width: 960px) {
+      .chat{
+        margin: 0;
+      }
+    }
+
+    /* These are the drawers */
+    .playground_drawer{
+      border-top-right-radius: 20px;
+      border-bottom-right-radius: 20px;
+    }
+    .friends_drawer{
+      border-top-left-radius: 20px;
+      border-bottom-left-radius: 20px;
+    }
+
+
+
+
+
+
+
+
+  /*
   .sheet_color{
     background-color: rgba(0, 0, 0, 0.4) !important;
   }
+  */
+
+
+
+ 
+
+  
+  
+  
 
 
 
