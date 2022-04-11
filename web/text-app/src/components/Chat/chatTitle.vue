2e5afe57c1e1a68e0b3275ce2ee64f2dcc834f9c
alekstrselicc <aleks.trselic@student.um.si>
2022-04-11 14:39:35 +0200

    titles are ready

--- web/text-app/src/components/Chat/chatTitle.vue
+++ web/text-app/src/components/Chat/chatTitle.vue
@@ -1,6 +1,16 @@
 <template>
-    <v-layout>
-        
+    <v-layout class="main_layout">
+        <!-- Background title--> 
+        <v-flex class="titles">
+            <h1 class="main_title">{{ title }}</h1>
+            <h1 class="sub_title">{{ subtitle }}</h1>
+        </v-flex>
+
+        <!-- Channel title-->
+        <v-flex class="settings_btn">
+            <v-btn icon><v-icon color="blue" large>mdi-wrench</v-icon></v-btn>
+        </v-flex>
+
     </v-layout>
 </template>
 
@@ -10,11 +20,40 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  
+   name: "chatTitle", 
+   data() {
+       return {
+           title: "Gaming",
+           subtitle: "Chill channel"
+       }
+   }
 }); 
 
 </script>
 
 <style>
 
+.main_layout{
+    min-width: 500px !important;
+}
+.titles{
+    height: 50px !important;
+    margin-left: 10px;
+    margin-top: 20px;
+}
+.settings_btn{
+    position: absolute;
+    margin-left: 170px;
+    margin-top: 36px;
+}
+
+.main_title{
+    font-size: 40px;
+    color: white;
+}
+.sub_title{
+    font-size: 20px !important;
+    color: white;
+    margin-left: 4px;
+}
 </style>
