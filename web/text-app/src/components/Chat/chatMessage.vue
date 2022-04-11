d25ab584a4bb792ab6e41e1ebf659d52fb6ece61
alekstrselicc <aleks.trselic@gmail.com>
2022-04-11 22:32:04 +0200

    added send message bar and made some fixes

--- web/text-app/src/components/Chat/chatMessage.vue
+++ web/text-app/src/components/Chat/chatMessage.vue
@@ -1,8 +1,17 @@
 <template>
-    <v-layout justify-center>
-        
-        
-    </v-layout>
+    <div class="main_box">
+    <v-text-field outlined class="send_box" label="Message...">
+      <v-btn icon>
+      <v-icon
+        color="white"
+        class="outlined"
+        large
+      >
+        mdi-arrow-right
+      </v-icon>
+        </v-btn>
+    </v-text-field>
+    </div>
 </template>
 
 
@@ -17,5 +26,32 @@ export default Vue.extend({
 </script>
 
 <style>
+.main_box{
+    width: 100% !important;
+    margin-top: -100px;
+}
+
+.v-icon.outlined {
+    border: 1px solid blue;
+    background: blue; 
+    border-radius:50%;
+    height: 46px;
+    width: 46px;
+}
+.send_box{
+    height: 20px !important; 
+    border-radius: 30px; 
+    font-size: 20px;
+    width: 100% !important;
+}
+@media (max-width: 960px) {
+
+.main_box{
+    margin-bottom: 140px !important;
+}
+
+}
+
+
 
 </style>
