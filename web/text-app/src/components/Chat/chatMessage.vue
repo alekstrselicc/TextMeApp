eb94e5d7cac031b43a0ad48e9c8df869ebd20b17
alekstrselicc <aleks.trselic@student.um.si>
2022-04-12 13:30:01 +0200

    send message bar is ready, fixed some bugs

--- web/text-app/src/components/Chat/chatMessage.vue
+++ web/text-app/src/components/Chat/chatMessage.vue
@@ -1,57 +1,62 @@
 <template>
-    <div class="main_box">
-    <v-text-field outlined class="send_box" label="Message...">
-      <v-btn icon>
-      <v-icon
-        color="white"
-        class="outlined"
-        large
-      >
-        mdi-arrow-right
-      </v-icon>
-        </v-btn>
-    </v-text-field>
-    </div>
+  <div class="main_box">
+    <v-text-field
+      class="search-input"
+      solo
+      hide-details
+      rounded
+      elevation-12
+      placeholder="Message..."
+      aria-label="Search"
+      append-icon="mdi-arrow-right"
+      @click:append="gridView = !gridView"
+    />
+  </div>
 </template>
 
-
 <script lang="ts">
-
 import Vue from "vue";
 
-export default Vue.extend({
-  
-}); 
-
+export default Vue.extend({});
 </script>
 
 <style>
-.main_box{
-    width: 100% !important;
-    margin-top: -100px;
+.main_box {
+  width: 100% !important;
+  margin-top: -80px;
 }
-
 .v-icon.outlined {
-    border: 1px solid blue;
-    background: blue; 
-    border-radius:50%;
-    height: 46px;
-    width: 46px;
+  border: 1px solid #007abe !important;
+  background: #007abe !important;
+  border-radius: 50%;
+  height: 46px !important;
+  width: 46px !important;
+  position: absolute;
 }
-.send_box{
-    height: 20px !important; 
-    border-radius: 30px; 
-    font-size: 20px;
-    width: 100% !important;
+
+.send_box {
+  height: 56px !important;
+  border-radius: 30px;
+  font-size: 20px;
+  width: 100% !important;
+  background-color: white !important;
 }
+.main_box .v-input__icon--append .v-icon { 
+    color: white !important;
+    font-size: 30px !important;
+    border-radius: 50%;
+    min-width: 46px !important;
+    height: 45px !important;
+    margin-left: 15px;
+    background-color: #007abe !important;
+}
+
 @media (max-width: 960px) {
-
-.main_box{
-    margin-bottom: 140px !important;
+  .main_box {
+    position: absolute;
+    bottom: 0;
+    margin-bottom: 30px;
+    width: 90% !important;
+  }
 }
-
-}
-
-
-
 </style>
