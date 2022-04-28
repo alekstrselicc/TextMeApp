574818a8a1442f2c8b4e30fd055b1c8d6257c60a
Vid Koblar <vidkoblar@Vids-MacBook-Pro.local>
2022-04-28 09:50:08 +0200

    add birth, phone, country, email conf to register. Add gender and relationship post method. Add routes.

--- backend/example-app/app/Http/Controllers/AuthController.php
+++ backend/example-app/app/Http/Controllers/AuthController.php
@@ -15,15 +15,23 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $fields = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|confirmed'
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|string|confirmed|unique:users,email',
+            'password' => 'required|string|confirmed',
+            'country' => 'required|string',
+            'phone' => 'required|string',
+            'birth' => 'required|date'
         ]);
 
         $user = User::create([
-            'first_name' => $fields['name'],
+            'first_name' => $fields['first_name'],
+            'last_name' => $fields['last_name'],
             'email' => $fields['email'],
-            'password' => bcrypt($fields['password'])
+            'password' => bcrypt($fields['password']),
+            'country' => $fields['country'],
+            'phone' => $fields['phone'],
+            'birth' => $fields['birth'],
         ]);
 
         $token = $user->createToken('keytoken');
--- backend/example-app/routes/api.php
+++ backend/example-app/routes/api.php
@@ -48,20 +48,20 @@ Route::post('/login', 'App\Http\Controllers\AuthController@login');
 //in progress:
 
 //protected routes
-route::apiResource('playgrounds', PlaygroundController::class)->only(['index','show','store','update','destroy']);
 Route::group(['middleware' => ['auth:api']], function () {
         
         route::apiResource('private_chats', PrivateChatController::class)->only(['index','store','destroy']);
         route::apiResource('user_type', UserTypeController::class)->only(['index','show','store','update','destroy']);
         route::apiResource('channels', ChannelController::class)->only(['index','show','store','update','destroy']);
         route::apiResource('status', StatusController::class)->only(['index','show','update']);
-        route::apiResource('relationship', RelationshipController::class)->only(['index','show','update']);
+        route::apiResource('relationship', RelationshipController::class)->only(['index','show','store','update']);
         route::apiResource('language', LanguageController::class)->only(['index','show','store']);
         route::apiResource('theme', ThemeController::class)->only(['index','show']);
         route::apiResource('country', CountryController::class)->only(['index','show','store','update','destroy']);
-        route::apiResource('gender', GenderController::class)->only(['index','show','destroy']);
+        route::apiResource('gender', GenderController::class)->only(['index','show','store','destroy']);
         route::apiResource('user', UserController::class)->only(['index','show','update','destroy']);
         route::apiResource('participants', ParticipantsController::class)->only(['index','show']);
+        route::apiResource('playgrounds', PlaygroundController::class)->only(['index','show','store','update','destroy']);
         Route::get('/logout', [AuthController::class, 'logout']);
         route::apiResource('messages', MessageController::class)->only(['index','show','store','destroy']);
         route::apiResource('towns', TownController::class)->only(['index','show','store','update','destroy']);
